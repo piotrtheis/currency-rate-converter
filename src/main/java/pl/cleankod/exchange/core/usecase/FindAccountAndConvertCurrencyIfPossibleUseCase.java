5eef3b66b5392320901a4164f2f364ecc0b0ec54
Piotr Theis <piotr.theis@escola.pl>
2022-03-21 23:51:10 +0100

    Result based error handling

--- src/main/java/pl/cleankod/exchange/core/usecase/FindAccountAndConvertCurrencyIfPossibleUseCase.java
+++ src/main/java/pl/cleankod/exchange/core/usecase/FindAccountAndConvertCurrencyIfPossibleUseCase.java
@@ -1,10 +1,12 @@
 package pl.cleankod.exchange.core.usecase;
 
 import pl.cleankod.exchange.core.domain.Account;
+import pl.cleankod.util.domain.Result;
 
 import java.util.Currency;
 import java.util.Optional;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class FindAccountAndConvertCurrencyIfPossibleUseCase {
 
@@ -16,25 +18,71 @@ public class FindAccountAndConvertCurrencyIfPossibleUseCase {
         this.findAndConvertUseCase = findAndConvertUseCase;
     }
 
-    public Optional<Account> execute(Account.Id id, String targetCurrency) {
-        return Optional.ofNullable(targetCurrency)
-                .filter(Predicate.not(String::isBlank))
-                .map(s ->
-                        findAndConvertUseCase.execute(id, Currency.getInstance(s))
-                )
-                .orElseGet(() ->
-                        findAccountUseCase.execute(id)
-                );
+    public Result<Account, FailedReason> execute(Account.Id id, String targetCurrency) {
+        var currency = getCurrentInstance(targetCurrency);
+
+        if (currency.isEmpty()) {
+            return find(id);
+        }
+
+        return convert(id, currency.get());
     }
 
-    public Optional<Account> execute(Account.Number number, String targetCurrency) {
+    public Result<Account, FailedReason> execute(Account.Number number, String targetCurrency) {
+        var currency = getCurrentInstance(targetCurrency);
+
+        if (currency.isEmpty()) {
+            return find(number);
+        }
+
+        return convert(number, currency.get());
+    }
+
+    private Result<Account, FailedReason> find(Account.Id id) {
+        return findAccountUseCase.execute(id)
+                .map(Result::<Account, FailedReason>successful)
+                .orElseGet(() -> Result.fail(FailedReason.MISSING_ACCOUNT));
+    }
+
+    private Result<Account, FailedReason> find(Account.Number number) {
+        return findAccountUseCase.execute(number)
+                .map(Result::<Account, FailedReason>successful)
+                .orElseGet(() -> Result.fail(FailedReason.MISSING_ACCOUNT));
+    }
+
+    private Result<Account, FailedReason> convert(Account.Id id, Currency currency) {
+        return handleErrors(() -> findAndConvertUseCase.execute(id, currency)
+                .map(Result::<Account, FailedReason>successful)
+                .orElseGet(() -> Result.fail(FailedReason.MISSING_ACCOUNT))
+        );
+    }
+
+    private Result<Account, FailedReason> convert(Account.Number number, Currency currency) {
+        return handleErrors(() -> findAndConvertUseCase.execute(number, currency)
+                    .map(Result::<Account, FailedReason>successful)
+                    .orElseGet(() -> Result.fail(FailedReason.MISSING_ACCOUNT))
+        );
+    }
+
+    private Result<Account, FailedReason> handleErrors(Supplier<Result<Account, FailedReason>> supplier) {
+        try {
+            return supplier.get();
+        } catch (CurrencyConversionException e) {
+            return Result.fail(FailedReason.UNSUPPORTED_CURRENCY);
+        } catch (RuntimeException e) {
+            return Result.fail(FailedReason.UNKNOWN);
+        }
+    }
+
+    private Optional<Currency> getCurrentInstance(String targetCurrency) {
         return Optional.ofNullable(targetCurrency)
                 .filter(Predicate.not(String::isBlank))
-                .map(s ->
-                        findAndConvertUseCase.execute(number, Currency.getInstance(s))
-                )
-                .orElseGet(() ->
-                        findAccountUseCase.execute(number)
-                );
+                .map(Currency::getInstance);
+    }
+
+    public static enum FailedReason {
+        MISSING_ACCOUNT,
+        UNSUPPORTED_CURRENCY,
+        UNKNOWN
     }
 }
--- src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
+++ src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
@@ -1,9 +1,13 @@
 package pl.cleankod.exchange.entrypoint;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.cleankod.exchange.core.domain.Account;
 import pl.cleankod.exchange.core.usecase.FindAccountAndConvertCurrencyIfPossibleUseCase;
+import pl.cleankod.exchange.core.usecase.FindAccountAndConvertCurrencyIfPossibleUseCase.FailedReason;
+import pl.cleankod.exchange.entrypoint.model.ApiError;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -19,18 +23,26 @@ public class AccountController {
     }
 
     @GetMapping(path = "/{id}")
-    public ResponseEntity<Account> findAccountById(@PathVariable String id, @RequestParam(required = false) String currency) {
+    public ResponseEntity<?> findAccountById(@PathVariable String id, @RequestParam(required = false) String currency) {
         return findAccountAndConvertCurrencyUseCase.execute(Account.Id.of(id), currency)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .fold(ResponseEntity::ok, this::errorHandler);
     }
 
     @GetMapping(path = "/number={number}")
-    public ResponseEntity<Account> findAccountByNumber(@PathVariable String number, @RequestParam(required = false) String currency) {
+    public ResponseEntity<?> findAccountByNumber(@PathVariable String number, @RequestParam(required = false) String currency) {
         Account.Number accountNumber = Account.Number.of(URLDecoder.decode(number, StandardCharsets.UTF_8));
         return findAccountAndConvertCurrencyUseCase.execute(accountNumber, currency)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+                .fold(ResponseEntity::ok, this::errorHandler);
     }
 
+
+    private ResponseEntity<ApiError> errorHandler(FailedReason failedReason){
+        return switch (failedReason){
+            case MISSING_ACCOUNT -> ResponseEntity.notFound().build();
+            case UNKNOWN -> ResponseEntity.badRequest().build();
+            case UNSUPPORTED_CURRENCY -> ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiError("Cannot convert currency from EUR to PLN."));
+        };
+    }
 }
